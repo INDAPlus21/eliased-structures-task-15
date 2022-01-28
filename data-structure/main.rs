d77d9d0292dc76778a4cb8cd95e429687146edf1
MazeOfIth <76459089+MazeofIth@users.noreply.github.com>
2022-01-29 00:54:37 +0100

    freaking accepted

--- data-structure/main.rs
+++ data-structure/main.rs
@@ -78,71 +78,6 @@ impossible
 use std::io;
 use std::io::prelude::*;
 
-fn debug_determine_bag() {
-    let mut structure = vec![];
-    // let commands = vec![[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3]]; // queue
-    // let commands = vec![[1, 2], [1, 1], [2, 1], [2, 2]]; // stack
-    // let commands = vec![[1, 2], [1, 5], [1, 1], [1, 3], [2, 5], [1, 4], [2, 4]]; // priority queue
-    // let commands = vec![[1, 1], [1, 2], [1, 3], [2, 3], [2, 2], [2, 1]]; // not sure
-    let commands = vec![[1, 1], [2, 2]]; // impossible
-    let mut queue_order = vec![];
-    let mut stack_order = vec![];
-    let mut priority_order = vec![];
-    let mut is_stack = true;
-    let mut is_queue = true;
-    let mut is_priority = true;
-    for command in commands {
-        if command[0] == 1 {
-            structure.push(command[1]);
-            queue_order.push(command[1]);
-            stack_order.push(command[1]);
-            priority_order.push(command[1]);
-            eprintln!("{:?}", structure);
-        } else if command[0] == 2 {
-            let take_out = &command[1];
-            structure.retain(|e| e != &command[1]); // What if there are multiple of the same integers?
-            eprintln!("i took out {:?}", take_out);
-            let max_value = *priority_order.iter().max().unwrap();
-            eprintln!("max value: {:?}", max_value);
-            if command[1] == queue_order[0] {
-                eprintln!("queue not falsified");
-            } else {
-                is_queue = false;
-                eprintln!("queue falsified!!!");
-            }
-            if command[1] == stack_order[stack_order.len() - 1] {
-                eprintln!("stack not falsified");
-            } else {
-                is_stack = false;
-                eprintln!("stack falsified!!!");
-            }
-            if command[1] == max_value {
-                eprintln!("priority queue not falsified");
-            } else {
-                is_priority = false;
-                eprintln!("priority queue falsified!!!");
-            }
-            queue_order.remove(0);
-            stack_order.remove(stack_order.len() - 1);
-            let max_index = priority_order.iter().position(|&r| r == max_value).unwrap();
-            priority_order.remove(max_index);
-            eprintln!("queue_order {:?}", &queue_order);
-        }
-    }
-
-    if is_stack && !is_queue && !is_priority {
-        println!("stack");
-    } else if is_queue && !is_stack && !is_priority {
-        println!("queue");
-    } else if is_priority && !is_stack && !is_queue {
-        println!("priority queue");
-    } else if !is_priority && !is_stack && !is_queue {
-        println!("impossible");
-    } else {
-        println!("not sure");
-    }
-}
-
 fn main() {
     let input = io::stdin();
 
@@ -167,10 +102,11 @@ fn main() {
             .map(|s| s.parse().unwrap())
             .collect();
 
-        eprintln!("command: {:?}", command);
+        // eprintln!("command: {:?}", command);
+        // you can delete structure 
 
         if command.len() == 1 {
-            eprintln!("resetting");
+            // eprintln!("resetting");
             length_of_commands = command[0];
         } else if command[0] == 1 {
             structure.push(command[1]);
@@ -178,46 +114,46 @@ fn main() {
             stack_order.push(command[1]);
             priority_order.push(command[1]);
             line_iter += 1;
-            eprintln!("{:?}", structure);
+            // eprintln!("{:?}", structure);
         } else if command[0] == 2 {
-            if !structure.contains(&command[1]) {
+            if !priority_order.contains(&command[1]) && !queue_order.contains(&command[1]) && !stack_order.contains(&command[1]) {
                 is_stack = false;
                 is_queue = false;
                 is_priority = false;
             } else {
                 let take_out = &command[1];
                 structure.retain(|e| e != &command[1]); // What if there are multiple of the same integers?
-                eprintln!("i took out {:?}", take_out);
+                // eprintln!("i took out {:?}", take_out);
                 let max_value = *priority_order.iter().max().unwrap();
-                eprintln!("max value: {:?}", max_value);
+                // eprintln!("max value: {:?}", max_value);
                 if command[1] == queue_order[0] {
-                    eprintln!("queue not falsified");
+                    // eprintln!("queue not falsified");
                 } else {
                     is_queue = false;
-                    eprintln!("queue falsified!!!");
+                    // eprintln!("queue falsified!!!");
                 }
                 if command[1] == stack_order[stack_order.len() - 1] {
-                    eprintln!("stack not falsified");
+                    // eprintln!("stack not falsified");
                 } else {
                     is_stack = false;
-                    eprintln!("stack falsified!!!");
+                    // eprintln!("stack falsified!!!");
                 }
                 if command[1] == max_value {
-                    eprintln!("priority queue not falsified");
+                    // eprintln!("priority queue not falsified");
                 } else {
                     is_priority = false;
-                    eprintln!("priority queue falsified!!!");
+                    // eprintln!("priority queue falsified!!!");
                 }
                 queue_order.remove(0);
                 stack_order.remove(stack_order.len() - 1);
                 let max_index = priority_order.iter().position(|&r| r == max_value).unwrap();
                 priority_order.remove(max_index);
-                eprintln!("queue_order {:?}", &queue_order);
+                // eprintln!("queue_order {:?}", &queue_order);
             }
             line_iter += 1;
         }
 
-        eprintln!("line_iter: {}", line_iter);
+        // eprintln!("line_iter: {}", line_iter);
 
         if line_iter == length_of_commands && line_iter > 0 {
             if is_stack && !is_queue && !is_priority {
